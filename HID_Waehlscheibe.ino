181c3ef76185df906964c31b4ff5bca32e7e2cec
Karsten von Hornbostel <kaback@googlemail.com>
2018-09-11 17:53:22 +0200

    Aufhuebschungen

--- HID_Waehlscheibe.ino
+++ HID_Waehlscheibe.ino
@@ -1,8 +1,8 @@
 #include <Keyboard.h>
 #include <Bounce2.h>
 
-static const byte NSApin = 9;  // NSA switch is low when the dail disk is moved
-static const byte NSIpin = 10; // NSI when rotating back, nsi generates the impulses
+static const byte NSApin = 9;  // NSA, LOW wenn sich die Waehlscheibe dreht
+static const byte NSIpin = 10; // NSI, Erzeugt bei der Zuckbewegung der Waehlscheibe die Pulse
 
 Bounce waehlpulse = Bounce();
 Bounce scheibedreht = Bounce();
@@ -21,6 +21,7 @@ void setup()
   Keyboard.begin();
 	
 }
+
 unsigned int count=0;
 
 void loop()
@@ -28,24 +29,21 @@ void loop()
   waehlpulse.update();
   scheibedreht.update();
 
-  // Wenn sich die Waehlscheibe dreht...
-  if(scheibedreht.read() == LOW)
+                                        
+  if(scheibedreht.read() == LOW)    // Wenn sich die Waehlscheibe dreht...
   {
-    // ...und wenn ein Impuls anliegt
-    if(waehlpulse.fallingEdge()) {
-      // ... Impuls zaehlen
-      count++;
+                                       
+    if(waehlpulse.fallingEdge()) {  //... und wenn ein Impuls anliegt
+      count++;                      //... Impuls zaehlen
     }  
-  } else { // Wenn sich die Waehlscheibe nicht dreht...
-    if(count != 0) { //... und wenn wir vorher etwas gezaehlt haben
-      if(count == 10){ //... und wenn der Counter auf 10 steht
-  
-        Keyboard.write(48); //... schreibe eine 0 auf dem keyboard
-      } else { // wenn der Counter nicht 0 aber auch nicht 10 ist
-        Keyboard.write(48+count); //... schreibe den Counterwert auf dem Keyboard 
- 
+  } else {                          // Wenn sich die Waehlscheibe nicht dreht...
+    if(count != 0) {                //... und wenn wir vorher etwas gezaehlt haben
+      if(count == 10){              //... und wenn der Counter auf 10 steht
+        Keyboard.write(48);         //... schreibe eine 0 auf dem keyboard
+      } else {                      //... andernfalls, wenn der Counter nicht 0 aber auch nicht 10 ist
+        Keyboard.write(48+count);   //... schreibe den Counterwert auf dem Keyboard 
       }
-      count = 0; // Counter zuruecksetzen
+      count = 0;                    // Counter zuruecksetzen
     }
   }
 }
